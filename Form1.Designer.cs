03163b41ea3d2c4e4f25388d75cf0545754a5509
Tacotime2 <150088820+Tacotime2@users.noreply.github.com>
2024-04-27 17:18:09 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MapsTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gmapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gmapControl
+            // 
+            this.gmapControl.Bearing = 0F;
+            this.gmapControl.CanDragMap = true;
+            this.gmapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmapControl.GrayScaleMode = false;
+            this.gmapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmapControl.LevelsKeepInMemory = 5;
+            this.gmapControl.Location = new System.Drawing.Point(12, 12);
+            this.gmapControl.MarkersEnabled = true;
+            this.gmapControl.MaxZoom = 2;
+            this.gmapControl.MinZoom = 2;
+            this.gmapControl.MouseWheelZoomEnabled = true;
+            this.gmapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmapControl.Name = "gmapControl";
+            this.gmapControl.NegativeMode = false;
+            this.gmapControl.PolygonsEnabled = true;
+            this.gmapControl.RetryLoadTile = 0;
+            this.gmapControl.RoutesEnabled = true;
+            this.gmapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmapControl.ShowTileGridLines = false;
+            this.gmapControl.Size = new System.Drawing.Size(1238, 650);
+            this.gmapControl.TabIndex = 0;
+            this.gmapControl.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 674);
+            this.Controls.Add(this.gmapControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmapControl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using GMap.NET.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Attach event handlers
+            this.Load += new System.EventHandler(this.Form1_Load);
+            gmapControl.MouseClick += new MouseEventHandler(gmapControl_MouseClick);
+        }
+
+
+        GMapOverlay markersOverlay = new GMapOverlay("markers");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set the map to Google Satellite view
+            gmapControl.MapProvider = GMap.NET.MapProviders.GoogleSatelliteMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmapControl.Position = new GMap.NET.PointLatLng(43.380240, -95.147795);
+
+            //Configure the zoom settings
+            gmapControl.MinZoom = 5;
+            gmapControl.MaxZoom = 20;  //Higher zoom level for better satellite details
+            gmapControl.Zoom = 10;
+            gmapControl.CanDragMap = true;
+            gmapControl.DragButton = MouseButtons.Left;
+
+        }
+
+        private void AddMarker(double lat, double lng, string tooltipText)
+        {
+            var markersOverlay = new GMap.NET.WindowsForms.GMapOverlay("markers");
+            var marker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                new GMap.NET.PointLatLng(lat, lng),
+                GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_dot);
+
+            marker.ToolTipText = tooltipText;
+            markersOverlay.Markers.Add(marker);
+            gmapControl.Overlays.Add(markersOverlay);
+        }
+
+        private void gmapControl_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) //Checks if the right mouse button was clicked
+            {
+                var point = gmapControl.FromLocalToLatLng(e.X, e.Y);
+
+                //Coordinates on click
+                double lat = point.Lat;
+                double lng = point.Lng;
+
+                AddMarker(lat, lng, "New Marker");
+
+                gmapControl.Refresh();
+
+                MessageBox.Show($"Latitude: {lat}, Longitude: {lng}");
+            }
+        }
+    }
+}
